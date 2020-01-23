116f97f4c26535b503edce83fb5a96ccfa7ab4ba
alex.dana@epitech.eu <alex.dana@epitech.eu>
2020-01-23 19:54:35 +0100

    Pagionation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const limit = 25;
+  const [page, setPage] = useState(0);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const skip = page * limit;
+    Axios.get(
+      `https://leboncoin-api.herokuapp.com/api/offer/with-count?skip=${skip}&limit=${limit}`
+    ).then(response => {
+      setData(response.data);
+    });
+  }, [page]);
+
+  const setNewPage = nbr => {
+    setPage(nbr);
+  };
+
+  const generatePagination = () => {
+    const elem = [];
+    for (let i = 0; i < data.count / limit; i++) {
+      elem.push(
+        <button onClick={() => setNewPage(i)} key={i}>
+          {i}
+        </button>
+      );
+    }
+    return elem;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data && data.offers.length}
+      <div>
+        {data &&
+          data.offers.map((offer, index) => {
+            if (offer.pictures[0])
+              return <img key={index} alt="coucou" src={offer.pictures[0]} />;
+            return null;
+          })}
+      </div>
+      {data && generatePagination()}
     </div>
   );
 }
